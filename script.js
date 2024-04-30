4111cd4267555382178bceb745bf65b75332e72e
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-04-30 10:14:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+var timerElement = document.getElementById("timer");
+
+// Function to update date and time
+let currentDate = new Date();
+setInterval(function(){
+    currentDate = new Date();
+    timerElement.innerHTML = currentDate.toLocaleString();
+}, 1000);
